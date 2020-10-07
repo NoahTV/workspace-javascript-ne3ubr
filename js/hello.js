eae76eb85ca87e70fa36489831434282e2ee8e88
NoahTV <noahtv@usca.edu>
2020-10-07 13:13:09 -0400

    1 file modified

--- js/hello.js
+++ js/hello.js
@@ -1,1 +1,16 @@
-console.log("I am in the hello.js")
+console.log("I am in the hello.js");
+
+var today = new Date();
+var hoursNow = today.getHours();
+
+var greeting = ""
+if (hoursNow <= 12)
+{
+  greeting = "Good Morning"
+}
+else 
+{
+  greeting = "Good Afternoon"
+}
+
+document.write(greeting);
